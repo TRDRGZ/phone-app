f42bcc8d7615835318cd994a8a6ee0bd6ac8dfd3
26ryss <26ryss@gmail.com>
2024-06-13 15:12:06 +0900

    画面を作成

---
+++ app/app.py
@@ -0,0 +1,26 @@
+#Flaskとrender_template（HTMLを表示させるための関数）をインポート
+from flask import Flask,render_template
+from flask import Flask, request, jsonify
+from subprocess import Popen, PIPE
+
+#Flaskオブジェクトの生成
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/start_audio_stream', methods=['GET'])
+def start_audio_stream():
+    port = request.args.get('port')
+    ip_address = request.args.get('ip')
+
+    if ip_address:
+        cmd = f"rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone {ip_address} {port} | play -t raw -b 16 -c 1 -e s -r 44100 -"
+    else:
+        cmd = f"rec -t raw -b 16 -c 1 -e s -r 44100 - | ./phone {port} | play -t raw -b 16 -c 1 -e s -r 44100 -"
+    
+    # 実行可能ファイルを起動
+    process = Popen(cmd, shell=True)
+
+    return jsonify(message="Audio Stream started"), 200
